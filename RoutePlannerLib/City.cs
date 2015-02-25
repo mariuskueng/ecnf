68c4fa808145416fbcc2c4a52fd3dd945e72b697
Marius Küng <apps@mariuskueng.ch>
2015-02-25 12:11:25 +0100

    Added city class

---
+++ RoutePlannerLib/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Population { get; set; }
+        public WayPoint Location { get; set; }
+
+        public City (string n, string c, int p, double lat, double lon)
+        {
+            this.Name = n;
+            this.Country = c;
+            this.Population = p;
+            this.Location = new WayPoint(n, lat, lon);
+        }
+    }
+}
+
