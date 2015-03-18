c069f9b7214a4b8e5b41f476ab0e21284d3f1665
Marius Küng <apps@mariuskueng.ch>
2015-03-18 09:41:31 +0100

    Moved TextReaderExtension.cs, fixed reader
    Moved TextReaderExtension.cs to RoutePlannerLib subdirectory

--- RoutePlannerLib/Cities.cs
+++ RoutePlannerLib/Cities.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 {
---
+++ RoutePlannerLib/Util/TextReaderExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
+{
+    public static class TextReaderExtension
+    {
+        public static IEnumerable<string[]> GetSplittedLines(this TextReader reader, char separationChar)
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                yield return line.Split(separationChar);
+            }
+        }
+    }
+}
