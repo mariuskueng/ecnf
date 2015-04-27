460cefa3b25ecaa9f19182f4810bf1e46d061f8f
Yves Buschor <yves.buschor@gmail.com>
2015-04-27 17:14:30 +0200

    Lab 7 finished

---
+++ RoutePlannerLib/Dynamic/World.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Dynamic
+{
+    public class World : DynamicObject
+    {
+        private Cities cities;
+        public World(Cities c)
+        {
+            cities = c;
+        }
+        public override bool TryInvokeMember(InvokeMemberBinder binder, Object[] args, out Object result)
+        {
+            City city = cities.FindCity(binder.Name);
+            if (city != null)
+            {
+                result = city;
+            }
+            else
+            {
+                result = String.Format("The city \"{0}\" does not exist!", binder.Name);
+            }
+
+            return true;
+        }
+    }
+}
