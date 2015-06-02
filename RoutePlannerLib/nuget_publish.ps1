c19a65aaa1bd643b531a80c67ec7a1c53c153fe3
Yves Buschor <yves.buschor@gmail.com>
2015-06-03 00:06:10 +0200

    improved powerscript wildcard
    probably still not ideal, but better ;)

--- RoutePlannerLib/nuget_publish.ps1
+++ RoutePlannerLib/nuget_publish.ps1
@@ -1,8 +1,8 @@
 Remove-Item *.nupkg
 
 nuget pack RoutePlannerLib.csproj -Build -Symbols -Properties Configuration=Release
-nuget push FHNW.ecnf.RoutePlannerLib.*.nupkg e9c479e2-9ef4-447d-8498-6237040e8c35
-nuget push FHNW.ecnf.RoutePlannerLib.*.symbols.nupkg e9c479e2-9ef4-447d-8498-6237040e8c35
+
+nuget push FHNW.ecnf.RoutePlannerLib.?.?.?.?.nupkg e9c479e2-9ef4-447d-8498-6237040e8c35
 
 Write-Host -NoNewLine 'Process finished. Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
